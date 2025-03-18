205bf45d5244e172722c375a33cdaec74ae51d0e
TanwaratSalee <tanwas.17@gmail.com>
2025-03-18 15:45:22 +0700

    gps tracking

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,30 @@
 
 import { useEffect, useState } from "react";
 
+// Define the type for card data
+type CardData = {
+  src: string;
+  title: string;
+  link: string;
+};
+
 export default function Home() {
-  const cardData = [
+  // Explicitly type the cardData array
+  const cardData: CardData[] = [
     { src: "/img/KeyAccount.png", title: "KeyAccount", link: "https://lite.cpall.co.th/Logistic/keyaccount/login.php" },
-    { src: "/img/FleetBusiness.png", title: "Daily Fleet Management", link: "http://10.182.32.115:3000/#" },
-    { src: "/img/Master-data.png", title: "MASTER DATA", link: "http://10.182.32.115:3000/#" },
-    { src: "/img/goCRM.png", title: "goCRM", link: "http://10.182.32.115:3000/#" },
-    { src: "/img/gps.png", title: "GPS Tracking", link: "http://10.182.32.115:3000/#" },
-    { src: "/img/send-it.png", title: "sendit", link: "http://10.182.32.115:3000/#" },
-    { src: "/img/MMMOP.png", title: "MMMOP", link: "http://10.182.32.115:3000/#" },
-    { src: "/img/WTS.png", title: "Warehouse Terminal System", link: "http://10.182.32.115:3000/#" },
-    { src: "/img/transport.png", title: "Transportation", link: "http://10.182.32.115:3000/#" }
+    { src: "/img/FleetBusiness.png", title: "Daily Fleet Management", link: "https://lite.cpall.co.th/Logistic/vendor_portal/login.php" },
+    { src: "/img/Master-data.png", title: "MASTER DATA", link: "http://10.184.9.26:8080/NEW_RPA/login.phphttp://10.184.9.26:8080/NEW_RPA/login.php" },
+    { src: "/img/goCRM.png", title: "goCRM", link: "http://gocrmcss.gosoft.co.th/crm/signInAD.jsp" },
+    { src: "/img/gps.png", title: "GPS Tracking", link: "" },
+    { src: "/img/send-it.png", title: "sendit", link: "https://manage.sendit.asia/th/2stage/login" },
+    { src: "/img/MMMOP.png", title: "MMMOP", link: "https://mms.cpall.co.th/masters/servlet/masters" },
+    { src: "/img/WTS.png", title: "Warehouse Terminal System", link: "" },
+    { src: "/img/transport.png", title: "Transportation", link: "http://172.29.113.15/Logistic/transportation/apps/pages/login.php" }
   ];
 
   const [currentTime, setCurrentTime] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedGpsLink, setSelectedGpsLink] = useState("");
 
   useEffect(() => {
     const updateClock = () => {
@@ -37,6 +47,22 @@ export default function Home() {
     const interval = setInterval(updateClock, 1000);
     return () => clearInterval(interval);
   }, []);
+  
+
+  const handleGpsClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  const handleLinkSelection = (link: string) => {
+    setSelectedGpsLink(link);
+    setShowPopup(false);
+    //window.location.href = link; // same window
+    window.open(link, "_blank"); // new window
+  };
 
   return (
     <>
@@ -48,11 +74,12 @@ export default function Home() {
       `}</style>
 
       {/* Background */}
-      <div className="flex flex-col min-h-screen bg-cover bg-center bg-opacity-60 relative" style={{ backgroundImage: "url('/img/backgroung.png')" }}>
+      <div className="flex flex-col min-h-screen bg-cover bg-center relative"
+      style={{ backgroundImage: "url('/img/backgroung.png')" }}>
 
         {/* Navbar */}
         <div className="navbar bg-base-100 shadow-sm fixed top-0 left-0 w-full z-50 flex justify-between items-center px-4 md:px-10">
-          <div className="hidden md:flex items-center">
+          {/* <div className="hidden md:flex items-center">
             <div className="relative">
               <input type="checkbox" id="menu-toggle" className="peer hidden" />
               <label htmlFor="menu-toggle" className="btn btn-ghost btn-circle">
@@ -69,8 +96,8 @@ export default function Home() {
                 </ul>
               </div>
             </div>
-          </div>
-          <div className="text-xl font-kanit">Transport Data Center</div>
+          </div> */}
+          <div className="text-xl font-kanit uppercase">Transport Data Center</div>
           <div className="hidden md:flex text-normal font-normal pr-3 font-kanit">{currentTime}</div>
         </div>
 
@@ -84,8 +111,8 @@ export default function Home() {
             <div className="sm:col-span-4 content-center">
               <div className="grid grid-cols-2 sm:grid-cols-3 sm:gap-2 md:mr-6 2xl:grid-cols-4">
                 {cardData.map((item, index) => (
-                  <a key={index} href={item.link || "#"} target="_blank" rel="noopener noreferrer">
-                    <div className="bg-white relative group overflow-hidden rounded-lg shadow-md flex flex-col items-center m-2">
+                  <a key={index} href={item.link || "#"} target={item.link ? "_blank" : ""} rel="noopener noreferrer">
+                    <div className="bg-white relative group overflow-hidden rounded-lg shadow-md flex flex-col items-center m-2" onClick={item.title === "GPS Tracking" ? handleGpsClick : undefined}>
                       <img src={item.src} alt={item.title} className="h-30 w-72 object-cover transition-transform duration-300 group-hover:scale-110" />
                       <div className="bg-gray-200 w-full text-center py-1 font-semibold text-gray-700 text-sm md:text-base py-2">
                         {item.title}
@@ -109,18 +136,44 @@ export default function Home() {
           </div>
         </div>
 
+        {/* GPS Tracking Popup */}
+        {showPopup && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-80">
+            <div className="flex flex-row justify-between">
+              <h3 className="text-lg font-medium mb-4">Select GPS Tracking Service</h3>
+              <button onClick={handleClosePopup} className=" btn btn-sm btn-circle">
+                ✖
+              </button>
+            </div>
+            <div className="flex flex-row justify-between">
+              {/* DTC Option with Image */}
+              <div className="basis-auto mb-4 text-center bg-white shadow-md rounded-b-lg">
+                <img src="/img/dtc-logo.png" alt="DTC Logo" className="w-32 h-28 mx-auto mb-2 object-cover" />
+                <a href="https://gp.com" className="block w-full text-center p-2 bg-gray-200 text-black rounded-b-lg ">DTC</a>
+              </div>
+        
+              {/* Tele Tec Option with Image */}
+              <div className="basis-auto mb-4 text-center bg-white shadow-md rounded-b-lg">
+                <img src="/img/teletec-logo.png" alt="Tele Tec Logo" className="w-32 h-28 mx-auto mb-2 object-cover" />
+                <a href="https://www.tel.com/" className="block w-full text-center p-2 bg-gray-200 text-black rounded-b-lg">Tele Tec</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        )}
+
         {/* Footer */}
         <footer className="bg-gray-200 text-center p-4 shadow-md w-full mt-auto relative">
           <div className="container mx-auto flex justify-between items-center">
-            <div className="text-gray-500">
-              <strong>
-                Power by improvement transportation team
-              </strong>
+            <div className="text-gray-500 font-medium">
+              Power by improvement transportation team
             </div>
             <div className="text-gray-50 text-gray-200">Anything you want</div>
           </div>
         </footer>
-        
+
       </div>
     </>
   );
